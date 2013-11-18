c9a5eeac2fae09d2287b277346b8969fe0e7ca17
Ryan VanGundy <ryan.vangundy@gmail.com>
2013-11-18 16:02:34 -0500

    Added basic project files

---
+++ test/test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var assert = chai.assert;
+var animate = require('../animate');
+
+describe('animate()', function() {
+
+    it('is a function', function() {
+        assert.ok(typeof animate === 'function');
+    });
+});
